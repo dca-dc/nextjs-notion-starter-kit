48cdbf255f81d101513c03c923b593ea36f6fcae
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-10-17 12:58:51 +0100

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,1 +1,12 @@
+import * as React from 'react'
 
+import styles from './styles.module.css'
+
+export const GitHubShareButton: React.FC = () => {
+  return (
+    <a
+      aria-label='Site by Specian Tech Services'
+    >
+    </a>
+  )
+}
